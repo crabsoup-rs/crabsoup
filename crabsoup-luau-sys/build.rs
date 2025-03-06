47b6d2700e3ffeaa4ebfb2daf92d9445b19c44b7
Rimi Kanokawa <rimi@rimin.moe>
2025-03-06 03:51:16 -0800

    Update luau

--- crabsoup-luau-sys/build.rs
+++ crabsoup-luau-sys/build.rs
@@ -5,18 +5,28 @@ fn main() {
     let analysis_dir = source_dir_base.join("luau").join("Analysis");
     let ast_dir = source_dir_base.join("luau").join("Ast");
     let common_dir = source_dir_base.join("luau").join("Common");
+    let compiler_dir = source_dir_base.join("luau").join("Compiler");
     let config_dir = source_dir_base.join("luau").join("Config");
+    let eqsat_dir = source_dir_base.join("luau").join("EqSat");
+    let vm_dir = source_dir_base.join("luau").join("VM");
 
     cc::Build::new()
         .cpp(true)
         .warnings(false)
         .flag_if_supported("-std=c++17")
+        .define("LUA_API", "extern \"C\"")
+        .define("LUACODE_API", "extern \"C\"")
+        .define("LUACODEGEN_API", "extern \"C\"")
         .add_files_by_ext(&analysis_dir.join("src"), "cpp")
         .add_files_by_ext(&config_dir.join("src"), "cpp")
+        .add_files_by_ext(&eqsat_dir.join("src"), "cpp")
         .include(&analysis_dir.join("include"))
         .include(&ast_dir.join("include"))
         .include(&common_dir.join("include"))
+        .include(&compiler_dir.join("include"))
         .include(&config_dir.join("include"))
+        .include(&eqsat_dir.join("include"))
+        .include(&vm_dir.join("include"))
         .compile("luauanalysis");
     println!("cargo:rerun-if-changed=luau");
 }
